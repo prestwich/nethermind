7ca0e5b89b4e0acfd5a03c13001d22285a4d9434
James Prestwich <prestwich@clabs.co>
2020-09-23 11:32:23 -0700

    feature: eip2539 prep

--- src/Nethermind/Nethermind.Crypto/ShamatarLib.cs
+++ src/Nethermind/Nethermind.Crypto/ShamatarLib.cs
@@ -30,6 +30,17 @@ namespace Nethermind.Crypto.Bls
             byte* error,
             ref int errorLength);
 
+        [DllImport("shamatar")]
+        private static extern unsafe uint eip2539_perform_operation(
+            byte operation,
+            byte* input,
+            int inputLength,
+            byte* output,
+            ref int outputLength,
+            byte* error,
+            ref int errorLength);
+
+
         private static unsafe bool Bn256Op(byte operation, Span<byte> input, Span<byte> output)
         {
             int outputLength = output.Length;
@@ -48,7 +59,7 @@ namespace Nethermind.Crypto.Bls
             return externalCallResult == 0;
         }
 
-        private static unsafe bool BlsOp(byte operation, Span<byte> input, Span<byte> output)
+        private static unsafe bool Bls381Op(byte operation, Span<byte> input, Span<byte> output)
         {
             int outputLength = output.Length;
             int errorLength = 256;
@@ -60,6 +71,24 @@ namespace Nethermind.Crypto.Bls
             fixed (byte* errorPtr = &MemoryMarshal.GetReference(error))
             {
                 externalCallResult = eip2537_perform_operation(
+                    operation, inputPtr, input.Length, outputPtr, ref outputLength, errorPtr, ref errorLength);
+            }
+
+            return externalCallResult == 0;
+        }
+
+        private static unsafe bool Bls377Op(byte operation, Span<byte> input, Span<byte> output)
+       {
+            int outputLength = output.Length;
+            int errorLength = 256;
+            uint externalCallResult;
+
+            Span<byte> error = stackalloc byte[errorLength];
+            fixed (byte* inputPtr = &MemoryMarshal.GetReference(input))
+            fixed (byte* outputPtr = &MemoryMarshal.GetReference(output))
+            fixed (byte* errorPtr = &MemoryMarshal.GetReference(error))
+            {
+                externalCallResult = eip2539_perform_operation(
                     operation, inputPtr, input.Length, outputPtr, ref outputLength, errorPtr, ref errorLength);
             }
 
@@ -83,47 +112,82 @@ namespace Nethermind.Crypto.Bls
 
         public static bool BlsG1Add(Span<byte> input, Span<byte> output)
         {
-            return BlsOp(1, input, output);
+            return Bls381Op(1, input, output);
         }
 
         public static bool BlsG1Mul(Span<byte> input, Span<byte> output)
         {
-            return BlsOp(2, input, output);
+            return Bls381Op(2, input, output);
         }
 
         public static bool BlsG1MultiExp(Span<byte> input, Span<byte> output)
         {
-            return BlsOp(3, input, output);
+            return Bls381Op(3, input, output);
         }
 
         public static bool BlsG2Add(Span<byte> input, Span<byte> output)
         {
-            return BlsOp(4, input, output);
+            return Bls381Op(4, input, output);
         }
 
         public static bool BlsG2Mul(Span<byte> input, Span<byte> output)
         {
-            return BlsOp(5, input, output);
+            return Bls381Op(5, input, output);
         }
 
         public static bool BlsG2MultiExp(Span<byte> input, Span<byte> output)
         {
-            return BlsOp(6, input, output);
+            return Bls381Op(6, input, output);
         }
 
         public static bool BlsPairing(Span<byte> input, Span<byte> output)
         {
-            return BlsOp(7, input, output);
+            return Bls381Op(7, input, output);
         }
 
         public static bool BlsMapToG1(Span<byte> input, Span<byte> output)
         {
-            return BlsOp(8, input, output);
+            return Bls381Op(8, input, output);
         }
 
         public static bool BlsMapToG2(Span<byte> input, Span<byte> output)
         {
-            return BlsOp(9, input, output);
+            return Bls381Op(9, input, output);
+        }
+
+        public static bool Bls377G1Add(Span<byte> input, Span<byte> output)
+        {
+            return Bls377Op(1, input, output);
+        }
+
+        public static bool Bls377G1Mul(Span<byte> input, Span<byte> output)
+        {
+            return Bls377Op(2, input, output);
+        }
+
+        public static bool Bls377G1MultiExp(Span<byte> input, Span<byte> output)
+        {
+            return Bls377Op(3, input, output);
+        }
+
+        public static bool Bls377G2Add(Span<byte> input, Span<byte> output)
+        {
+            return Bls377Op(4, input, output);
+        }
+
+        public static bool Bls377G2Mul(Span<byte> input, Span<byte> output)
+        {
+            return Bls377Op(5, input, output);
+        }
+
+        public static bool Bls377G2MultiExp(Span<byte> input, Span<byte> output)
+        {
+            return Bls377Op(6, input, output);
+        }
+
+        public static bool Bls377Pairing(Span<byte> input, Span<byte> output)
+        {
+            return Bls377Op(7, input, output);
         }
     }
 }
